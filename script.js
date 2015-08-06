dc17d2538c1af6285c2995eebb757c80299114f3
max-borisov <max.borisov@yahoo.com>
2015-08-06 23:56:27 +0300

    Update settings js.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var mainTable,
     newPackageRow,
     packageTitleInput,
     packageTtnInput,
-    settingsHash;
+    settingsHash,
+    packageTTN;
 
 $(function() {
   $('table').on('click', 'td button', function(e) {
@@ -31,13 +32,18 @@ $(function() {
     settingsHash = [];
     $('table.table tr').each(function(index, elem) {
       if ($(elem).attr('class') != 'hidden') {
+        packageTTN = $(elem).find('td.package-ttn')[0].innerHTML
+        packageTTN = packageTTN.match(/\d{8,}/)
+        packageTTN = packageTTN[0]
         settingsHash.push({
           title: $(elem).find('td.package-title')[0].innerHTML,
-          ttn: $(elem).find('td.package-ttn')[0].innerHTML
+          ttn: packageTTN
         })
       }
     })
     console.log(settingsHash);
     location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(settingsHash));
+
+    // <a href=\"tel:59000012345\" x-apple-data-detectors=\"true\" x-apple-data-detectors-type=\"telephone\" x-apple-data-detectors-result=\"0\">59000012345</a>"
   })
 })
